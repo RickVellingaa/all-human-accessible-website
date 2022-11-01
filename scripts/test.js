8ab3258467df230774788d88e6d7aaac0e38338c
RickVellingaa <rickvellinga2408@gmail.com>
2022-11-01 20:30:08 +0100

    Verder gewerkt aan formulier

--- scripts/test.js
+++ scripts/test.js
@@ -2,59 +2,137 @@
 var Form1 = document.getElementById("form-one");
 var Form2 = document.getElementById("form-two");
 var Form3 = document.getElementById("form-three");
+var Form4 = document.getElementById("form-four");
+var Form5 = document.getElementById("form-five");
+var Form5A = document.getElementById("form-fiveA");
 
 // Vorige en volgende knoppen
 var Next1 = document.getElementById("nextstep-1");
 var Next2 = document.getElementById("nextstep-2");
+var Next3 = document.getElementById("nextstep-3");
+
 var Back1 = document.getElementById("prevstep-1");
 var Back2 = document.getElementById("prevstep-2");
+var Back3 = document.getElementById("prevstep-3");
+var Back4 = document.getElementById("prevstep-4");
+var Back5 = document.getElementById("prevstep-5");
 
 // Progress bar
 var progressBar = document.getElementById("progress-bar");
 
-// Voor het redirecten
-var apples = $('input[name="apples"]');
-var red = $('input[name="red"]');
-var green = $('input[name="green"]');
+// Voor het redirecten naar eerdere reacties vraag
+var redirectOne = $('input[name="redirect1"]');
+var redirectTwo = $('input[name="redirect2"]');
+
+// Voor het redirecten naar specifieke eerdere reacties vraag
+var redirectThree = $('input[name="redirect3"]');
+var redirectFour = $('input[name="redirect4"]');
+
+// Voor het redirecten naar eczeem
+var redirectFive = $('input[name="redirect5"]');
+var redirectSix = $('input[name="redirect6"]');
+var redirectSeven= $('input[name="bbb"]');
+
 
 Next1.onclick = function (){
   Form1.style.left = "-450px";
   Form2.style.left = "40px";
-  progressBar.style.left = "120px";
 }
 
 Back1.onclick = function (){
   Form1.style.left = "40px";
   Form2.style.left = "450px";
-  progressBar.style.left = "0px";
 }
 
 Next2.onclick = function (){
   Form2.style.left = "-450px";
   Form3.style.left = "40px";
-  progressBar.style.left = "240px";
 }
 
 Back2.onclick = function (){
   Form2.style.left = "40px";
   Form3.style.left = "450px";
-  progressBar.style.left = "120px";
+}
+
+Next3.onclick = function (){
+  Form3.style.left = "-450px";
+  Form4.style.left = "40px";
+}
+
+Back3.onclick = function (){
+  Form3.style.left = "40px";
+  Form4.style.left = "450px";
+}
+
+Back4.onclick = function (){
+  Form4.style.left = "40px";
+  Form5.style.left = "450px";
+}
+
+Back5.onclick = function (){
+  Form4.style.left = "40px";
+  Form5A.style.left = "450px";
 }
 
 function redirectPage(){
-    if ( apples.is(':checked') ) {
-        window.location.href="http://www.google.com" ;
-    } else if ( red.is(':checked') ) {
-        window.location.href="http://www.yahoo.com" ;
-    } else if ( green.is(':checked') ) {
-        window.location.href="http://www.aol.com" ;
+    if ( redirectOne.is(':checked') ) {
+        window.location.href="jong.html" ;
+    } else if ( redirectTwo.is(':checked') ) {
+        window.location.href="overleg.html" ;
+    } else if ( redirectThree.is(':checked') ) {
+      Form4.style.left = "-450px";
+      Form5.style.left = "40px";
+    } else if ( redirectFour.is(':checked') ) {
+      Form4.style.left = "-450px";
+      Form5A.style.left = "40px";
     }
 }
 
-function selectOnlyThis(id){
-    var myCheckbox = document.getElementsByName("myCheckbox");
-    Array.prototype.forEach.call(myCheckbox,function(el){
-      el.checked = false;
-    });
-    id.checked = true;
+function redirectPageEczeem(){
+  if ( redirectFive.is(':checked') ) {
+      window.location.href="overleg.html" ;
+  } else {
+    window.location.href="www.google.com" ;
   }
+}
+
+function redirectPageEczeemA(){
+  if ( redirectSix.is(':checked') ) {
+      window.location.href="overleg.html" ;
+  } else {
+    window.location.href="www.ex.com" ;
+  }
+}
+
+// Maar 1 vakje selecteren
+function selectOnlyThis(id) {
+  for (var i = 1;i <= 2; i++)
+  {
+      document.getElementById("Check" + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function selectOnlyThis1(id) {
+  for (var i = 1;i <= 4; i++)
+  {
+      document.getElementById("Redirect" + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function selectOnlyThis2(id) {
+  for (var i = 1;i <= 3; i++)
+  {
+      document.getElementById("CheckA" + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function selectOnlyThis3(id) {
+  for (var i = 1;i <= 3; i++)
+  {
+      document.getElementById("CheckB" + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}

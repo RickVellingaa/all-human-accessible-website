c9298d55f8a76682a24cf69e688969d5f52059a5
RickVellingaa <rickvellinga2408@gmail.com>
2022-11-03 20:45:14 +0100

    redirect pagina begin gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,14 @@ var bgImgHeight = document.getElementById('bgimg').style.height;
 if (bodyElHeight>bgImgHeight) {
     bgImgHeight.style.height=bodyElHeight;  
 }
+
+function shopFunction (){
+    window.location.href = "https://vinimini.nl/collections/frontpage/products/3-stappen-starter-kit";
+}
+function shopFunction1 (){
+    window.location.href = "https://vinimini.nl/collections/frontpage/products/6-stappen-starter-kit";
+}
+
+function shopFunctionAll (){
+    window.location.href = "https://vinimini.nl/collections/all";
+}
